d09ca5f2de109da8de3a50101d0110cf3a5cb3ce
Guilherme <syus13@gmail.com>
2023-04-24 18:27:21 -0300

    Atualização do projeto
    código para cadastro de usuário

--- Projeto Wexer PSI/assets/scripts/scriptCadastro.js
+++ Projeto Wexer PSI/assets/scripts/scriptCadastro.js
@@ -1,48 +1,26 @@
-//Função para adicionar uma nova linha na tabela
-function addToTable() {
+async function registerUser() {
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
 
-    //Definir as variaveis que receberão os dados
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('cpf').value;
-    let table = document.getElementById("tablePatient");
+    if (password !== confirmPassword) {
+      alert('As senhas não correspondem.');
+      return;
+    }
 
-    let tableSize = table.rows.length; //Calculando o tamanho da Tabela
-    let row = table.insertRow(tableSize); //Inserindo uma linha abaixo da Tabela
-    let cell1 = row.insertCell(0); //Inserindo as celulas da linha
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    
-    row.id = tableSize; //Adicionando o id no elemento a ser criado
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name,email, password })
+    };
 
-    //Criando o codigo do botão para remover a linha
-    let btnCode = "<button class='remove-btn' onclick='removeToTable(this)'>Remover</button>";
+    const response = await fetch('http://localhost:3000/patients', requestOptions);
 
-    //Preenchendo as celulas da linha
-    cell1.innerHTML = tableSize;
-    cell2.innerHTML = name;
-    cell3.innerHTML = email;
-    cell4.innerHTML = phone;
-    cell5.innerHTML = work;
-    cell6.innerHTML = btnCode;
-
-    //Limpando os campos de inserção de dados
-    document.getElementById('name').value = "";
-    document.getElementById('email').value = "";
-    document.getElementById('phone').value = "";
-    document.getElementById('work').value = "";
-
-    //Retornando 'false' para impedir o reload da pagina
-    return false;
-}
-
-//Função para remover uma linha
-function removeToTable(id){
-
-    let row = id.parentNode.parentNode.id; //Pegando o id do avô do botão
-    row = document.getElementById(row); //Recebendo o elemento da linha pelo ID
-    row.parentNode.removeChild(row); //Removendo a linha
-
-    //Retornando 'false' para impedir o reload da pagina
-    return false;
-}
+    if (!response.ok) {
+      const error = await response.text();
+      alert(`Ocorreu um erro: ${error}`);
+    } else {
+      alert('Usuário cadastrado com sucesso!');
+    }
+  }
